00e85f464054ac5d5c31ae73aa31c3a17481676d
Dung Nguyen <nddung1508@gmail.com>
2023-07-10 19:07:52 +0700

    [Feature] Creating Splash and Starting Screen

--- app/src/main/java/com/example/gofitness/ui/MainActivity.kt
+++ app/src/main/java/com/example/gofitness/ui/MainActivity.kt
@@ -24,6 +24,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun addFragment(fragment: Fragment) {
+        supportFragmentManager.inTransaction {
+            add(R.id.main_fragment_container, fragment)
+        }
+    }
+
     private inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
         val ft = beginTransaction()
         ft.func()
