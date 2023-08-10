2b60100092ae9e0bbf1e649eac1e11f9dd299b5d
Dung Nguyen <nddung1508@gmail.com>
2023-08-11 01:19:01 +0700

    Finish Setting up Room for Exercise

--- feature/home/src/main/java/com/example/home/HomePlanFragment.kt
+++ feature/home/src/main/java/com/example/home/HomePlanFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.data.AppDatabase
+import com.example.data.ExerciseViewModel
+import com.example.data.ExerciseViewModelFactory
 import com.example.home.databinding.FragmentHomePlanBinding
 
 class HomePlanFragment : Fragment() {
     lateinit var exerciseNavigator : ExerciseNavigator
     private lateinit var binding : FragmentHomePlanBinding
+    private lateinit var exerciseViewModel: ExerciseViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,5 +31,13 @@ class HomePlanFragment : Fragment() {
         binding.btnBack.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack()
         }
+        val appDatabase = AppDatabase.getInstance(requireContext())
+        val exerciseDao = appDatabase.exerciseDao()
+
+        val viewModelFactory = ExerciseViewModelFactory(exerciseDao)
+        exerciseViewModel = ViewModelProvider(this, viewModelFactory)[ExerciseViewModel::class.java]
+        exerciseViewModel.getAllExercises().observe(viewLifecycleOwner, Observer { exercises ->
+            Toast.makeText(requireContext(), exercises[0].name,Toast.LENGTH_LONG).show()
+        })
     }
 }
